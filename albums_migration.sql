2110fae4129d9a43a8a5118405f67d637d53d522
Luis Delgado <delgadoluis0510@gmail.com>
2019-10-10 11:13:05 -0500

    adding album seeder and select exercise from basic statements

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,11 +4,11 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums
 (
-    id           int unsigned    NOT NULL AUTO_INCREMENT,
-    artist       varchar(50)     NOT NULL,
-    name         varchar(50)     NOT NULL,
-    release_date int(4) unsigned NULL,
-    sales        float(5, 2)     NULL,
-    genre        varchar(50)     NULL,
+    id           int unsigned NOT NULL AUTO_INCREMENT,
+    artist       varchar(50)  NOT NULL,
+    name         varchar(50)  NOT NULL,
+    release_date int(4) unsigned,
+    sales        float(5, 2) unsigned,
+    genre        varchar(50),
     PRIMARY KEY (id)
 )
