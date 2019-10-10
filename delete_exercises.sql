b66ca1a8e35c222242f6d8d6cd68c6147497117a
Luis Delgado <delgadoluis0510@gmail.com>
2019-10-10 14:07:50 -0500

    added delete exercises from basic statements

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -4,22 +4,31 @@ use codeup_test_db;
 # from albums
 # where release_date > 1991;
 #
-# delete from albums where release_date > 1991;
+# delete
+# from albums
+# where release_date > 1991;
 #
-# select * from albums;
-
+# select *
+# from albums;
+#
 # select name as 'Albums with genre Disco:'
 # from albums
 # where genre = 'Disco';
 #
-# delete from albums where genre = 'Disco';
+# delete
+# from albums
+# where genre = 'Disco';
 #
-# select * from albums;
-
+# select *
+# from albums;
+#
 # select name as 'Albums by Whitney Houston'
 # from albums
 # where artist = 'Whitney Houston';
 #
-# delete from albums where artist = 'Whitney Houston';
+# delete
+# from albums
+# where artist = 'Whitney Houston';
 #
-# select * from albums;
+# select *
+# from albums;
