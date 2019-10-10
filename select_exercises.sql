ad7c639f24db5b7dd1bed682531d784e7faaa7e5
Luis Delgado <delgadoluis0510@gmail.com>
2019-10-10 11:49:56 -0500

    added update exercise from basic statements

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,26 +1,24 @@
 use codeup_test_db;
 
-# select *
-# from albums
-# where artist = 'Pink Floyd';
+select *
+from albums
+where artist = 'Pink Floyd';
 
-# select release_date
-# from albums
-# where name = 'Sgt. Peppers Lonely Hearts Club Band';
+select release_date
+from albums
+where name = 'Sgt. Peppers Lonely Hearts Club Band';
 
-# select genre
-# from albums
-# where name = 'Nevermind';
+select genre
+from albums
+where name = 'Nevermind';
 
-# select *
-# from albums
-# where release_date between 1990 and 2000;
-# select *
-# from albums
-# where sales < 20;
+select *
+from albums
+where release_date between 1990 and 2000;
+select *
+from albums
+where sales < 20;
 
 select *
 from albums
 where genre = 'Rock';
-# UPDATE EXERCISE
-# SELECT * FROM albums AS 'Albums';
