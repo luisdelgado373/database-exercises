4a0d33344a3e182555e706830dd13fc1e79f4800
Luis Delgado <delgadoluis0510@gmail.com>
2019-10-10 16:43:30 -0500

    adding order by exercise

---
+++ order_by_exercises.sql
@@ -0,0 +1,9 @@
+use employees;
+
+select * from employees where first_name in ('Irena','Vidya','Maya') order by first_name;
+select * from employees where first_name in ('Irena','Vidya','Maya') order by first_name, last_name;
+select * from employees where first_name in ('Irena','Vidya','Maya') order by last_name desc, first_name desc;
+
+select * from employees where last_name like 'E%' and last_name like '%E' order by emp_no desc;
+
+select * from employees where hire_date between '1990-01-01' and '1999-12-31' and birth_date like '%12-25%' order by birth_date, hire_date desc;
