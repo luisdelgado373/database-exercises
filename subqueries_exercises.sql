29bd788834902ef6437b6790ccd3df1d1240729d
Luis Delgado <delgadoluis0510@gmail.com>
2019-10-11 15:31:53 -0500

    adding bonus for subqueries from mysql

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -27,8 +27,32 @@ where emp_no in (
 select first_name as First, last_name as Last
 from employees
 where emp_no in (
-    select emp_no from dept_manager
+    select emp_no
+    from dept_manager
     where to_date = '9999-01-01'
-    )
-and gender = 'F';
+)
+  and gender = 'F';
 
+select dept_name as Department
+from departments
+where dept_no in (
+    select dept_no
+    from dept_manager
+    where emp_no in (
+        select emp_no
+        from employees
+        where gender = 'F'
+    )
+      and to_date = '9999-01-01')
+order by dept_name;
+
+select first_name as First, last_name as Last
+from employees
+where emp_no in (
+    select emp_no
+    from salaries
+    where salary in (
+        select MAX(salary)
+        from salaries
+    )
+);
